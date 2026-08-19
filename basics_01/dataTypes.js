8783be050b3d55dd8fbf42fb743d4f8aff0756b4
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-08-19 11:02:25 +0000

    learning about the data Types in java script

---
+++ basics_01/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";   // treat all js code as newer version
+
+// alert (3 + 3)  // we are using nodejs, not browser
+
+
+let name = "satyam"       // string
+let age = 21              // number or integer
+let isLoggedOut = false   // boolean
+
+// bigint  // for more numbers
+// null   // standalone value
+// undefine // 
+// symbol  // for uniqueness
+
+// object 
+
+console.log(typeof null)
+console.log(typeof name)
+console.log([typeof age, isLoggedOut])
