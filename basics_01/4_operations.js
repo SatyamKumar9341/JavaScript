7fa1c5b8679decd9c5bdced1568aaa54fc032043
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-08-19 15:06:52 +0000

    some case study on operations in javaScript

---
+++ basics_01/4_operations.js
@@ -0,0 +1,23 @@
+let value = 5
+let negValue = -value
+// console.log(negValue) 
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2**3);  2 to the power 3
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello "
+let str2 = "Satyam"
+
+let str = str1 + str2
+// console.log(str)
+
+
+console.log("10" + 2);
+console.log(1 + "12");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");   //  faaaaahhhh
+
