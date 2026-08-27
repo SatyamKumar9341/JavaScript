0fcc959f4f7a0e3d35a6602b3a293865a34248fa
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-08-27 15:15:26 +0000

    Some basics of functions and debugged for wrong output in line no23

--- basics_02/3_objects.js
+++ basics_02/3_objects.js
@@ -6,20 +6,20 @@
 const sym = Symbol("key");
 
 const JsUser = {
-    name : "satyam",
+    name : "Satyam",
     "full name" : "Satyam Kumar",
     [sym]: "This is a symbol value",
     age : 21,
     loaction : 'Dehradun',
     email : "satyam@google.com",
     isLoggedIn : false,
-    lastloggin : ["monday", "saturday"]
+    lastloggin : ["Monday", "Saturday"]
 }
 
 console.log(JsUser.email)
 console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[sym]);
+console.log(JsUser["full name"])  // some times it get's mandatory to access values by square brackets[] 
+console.log(JsUser[sym]);          // symbols can only be accessed by using sqare barckets 
 console.log(typeof sym)
 
 JsUser.email = "Satyam@microsoft.in"   // changes to object key values
@@ -27,3 +27,19 @@ console.log(JsUser.email)
 // Object.freeze(JsUser)                  // freezes the object jsUser => changes doesn't propagate
 JsUser.email = "satyam@outlook.com" 
 console.log(JsUser.email)              // doesn't change value for the key email
+console.log(JsUser)
+
+
+
+// functions 
+
+JsUser.greeting  = function(){
+    console.log("Hello Js User")
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this["full name"]}`)
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
