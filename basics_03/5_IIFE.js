0a9178721c5eb7e699e9cecde1fcf1e6427b018e
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-08-30 21:44:01 +0000

    IIFE definations and UseCases

--- basics_03/5_IIFE.js
+++ basics_03/5_IIFE.js
@@ -12,3 +12,20 @@
 ((name) => {
     console.log(`Chai pee loo, ${name}`)
 })("Satyam Kumar");
+
+
+
+
+// IIFE stands for: Immediately Invoked Function Expression
+// It is a function that is defined and executed immediately.
+
+// Syntax :-
+// (function () {
+//     console.log("Hello");
+// })();
+
+// ****** Why use IIFE? ******
+// 1. Avoid global scope pollution : Variables inside an IIFE stay private.
+
+
+// The semicolon in the END is important, JavaScript may otherwise treat the second IIFE as a continuation of the previous expression.
