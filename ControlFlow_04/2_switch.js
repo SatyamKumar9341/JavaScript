2dd065bc2a3dadd58c4f0687027859e69d844f9c
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-09-01 19:33:22 +0000

    Switch statement in java script with example code

--- ControlFlow_04/2_switch.js
+++ ControlFlow_04/2_switch.js
@@ -7,3 +7,28 @@
 //     default:
 //         break;
 // }
+
+const month = 3
+
+switch(month){
+    case 1: 
+     console.log("January")
+            break
+    case 2: console.log("Feb")
+            break
+    case 3: console.log("march")
+            break
+    case 4: console.log("April")
+            break
+    case 5: console.log("May")
+            break
+    case 6: console.log("June")
+            break
+    case 7: console.log("july")
+            break
+    case 8: console.log("August")
+            break
+    case 9: console.log("September")
+            break
+    default : console.log("Enter valid month number")        
+}
