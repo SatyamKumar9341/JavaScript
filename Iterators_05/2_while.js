7c09ee697a0660f84eb699457b339026e9b794d7
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-09-02 14:58:43 +0000

    while and do while loops in js

---
+++ Iterators_05/2_while.js
@@ -0,0 +1,24 @@
+
+// let n = 0
+// while(n <= 10){
+//     console.log(`value of index is ${n}`)
+//     n = n + 2
+// }
+
+
+let myArray = ['flsh', 'batman', 'superman']
+let i = 0
+while (i < myArray.length){
+    console.log(`heroe : ${myArray[i]}`)
+    i++
+}
+
+
+let score = 11
+do{
+    console.log(`score is : ${score}`)
+    score++
+ 
+}while(score <= 10);
+
+
