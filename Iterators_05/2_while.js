47607365471eba96bd143ae597dc95fdd1a89a17
Satyam kumar <152946582+SatyamKumar9341@users.noreply.github.com>
2026-09-02 15:03:03 +0000

    updations on do while loop

--- Iterators_05/2_while.js
+++ Iterators_05/2_while.js
@@ -14,11 +14,19 @@ while (i < myArray.length){
 }
 
 
-let score = 11
+let score = 1
 do{
     console.log(`score is : ${score}`)
-    score++
+    score = score + 1
  
 }while(score <= 10);
+
+
+let num = 11
+do{
+    console.log(`cuurent num is : ${num}`)
+    num++
+ 
+}while(num <= 10);
 
 
